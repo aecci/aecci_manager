4dcace093db1fc513424e868211ff1210953e06a
Oscar Quesada Webb <oskar.quesada30@gmail.com>
2023-04-03 17:02:30 -0600

    Add the default connection

--- auto_conf_VPN.sh
+++ auto_conf_VPN.sh
@@ -2,6 +2,8 @@
 # Script para configurar Proton VPN y compartir internet por alguna conexión utilizando
 # esta VPN
 
+default_connection="Wired connection 2"
+
 # Usa expect que es un programa para interactuar con programas interactivos
 if ! command -v expect &> /dev/null
 then echo "Installando programa expect..."
@@ -41,27 +43,33 @@ protonvpn-cli disconnect &> /dev/null
 protonvpn-cli netshield --ads-malware &> /dev/null
 protonvpn-cli connect --cc ES &> /dev/null
 
-output=$(nmcli -t -f NAME,TYPE con show | awk -F '[:\n]' '{ printf "%s\n", $1}')
-# Guarda todas las conexiónes en un arreglo
-readarray -t connections <<<"$output"
+if [ -n "$default_connection" ]; then
+    # Usar la conexión por defecto
+    nmcli -p connection modify "$selected_connection" ipv4.method shared &> /dev/null
+    echo "Ahora se está compartiendo internet con VPN a la conexión $default_connection"
+  else
+    output=$(nmcli -t -f NAME,TYPE con show | awk -F '[:\n]' '{ printf "%s\n", $1}')
+    # Guarda todas las conexiónes en un arreglo
+    readarray -t connections <<<"$output"
 
-# Muestra todas las conexiónes enumeradas
-echo "\nConexiónes disponibles"
-for i in "${!connections[@]}"; do
-    echo "$i. ${connections[$i]}"
-done
+    # Muestra todas las conexiónes enumeradas
+    echo -e "\nConexiónes disponibles"
+    for i in "${!connections[@]}"; do
+        echo "$i. ${connections[$i]}"
+    done
 
-echo -e "\nDeberia ser una conexión llamada [Wired connection 2]"
-read -p "Elige el numero de la conexión por la cuál compartir internet: " selection
+    echo -e "\nDeberia ser una conexión llamada [Wired connection 2]"
+    read -p "Elige el numero de la conexión por la cuál compartir internet: " selection
 
-# Valida la selección del usuario
-if ! [[ $selection =~ ^[0-9]+$ ]] || (( selection < 0 || selection >= ${#connections[@]} )); then
-    echo "Selección invalida"
-    exit 1
+    # Valida la selección del usuario
+    if ! [[ $selection =~ ^[0-9]+$ ]] || (( selection < 0 || selection >= ${#connections[@]} )); then
+        echo "Selección invalida"
+        exit 1
+    fi
+
+    selected_connection=${connections[$selection]}
+    # Use the selected connection
+    nmcli -p connection modify "$selected_connection" ipv4.method shared &> /dev/null
+    echo "Ahora se está compartiendo internet con VPN a la conexión $selected_connection"
 fi
 
-selected_connection=${connections[$selection]}
-# Use the selected connection
-nmcli -p connection modify "$selected_connection" ipv4.method shared &> /dev/null
-echo "Ahora se está compartiendo internet con VPN a la conexión $selected_connection"
-# echo "Sharing connection $selected_connection"
